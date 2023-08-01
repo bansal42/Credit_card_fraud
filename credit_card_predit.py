a7d44050a8ec2bd6d15352dc8daca14b80e263a6
Khushi Bansal <138283670+bansal42@users.noreply.github.com>
2023-08-01 22:24:51 +0530

    Add files via upload

---
+++ credit_card_predit.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+data = pd.read_csv("creditcard.csv")
+# data.info()
+y = data['Class']
+x = data.drop('Class', axis=1)
+x = x.drop('Time', axis=1)
+
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.4)
+
+rf=RandomForestClassifier()
+rf.fit(x_train,y_train)
+
+print(help(RandomForestClassifier))
+out6=rf.predict(x_test)
+
+print("Random Forest training score:", rf.score(x_train,y_train))
+print("Random Forest testing score:", accuracy_score(y_test,out6))
+
+pickle.dump(rf, open("model.pkl", "wb"))
